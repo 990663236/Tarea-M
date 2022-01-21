c8fe0c68fd607a28625e31be01c76d02b547ea5f
990663236 <83373886+990663236@users.noreply.github.com>
2022-01-21 00:44:07 -0500

    Agregar archivos de proyecto.

---
+++ Source.cpp
@@ -0,0 +1,33 @@
+//encuentra el promedio de n alumnos segun sus 4 notas ingresadas por teclado
+#include<iostream>
+using namespace std;
+int main() {
+
+	int n, menor, nota, suma;
+
+	float promedio;
+
+	cout << "\ningrese la cantidad de alumnos: ";
+	cin >> n;
+	for (int j = 0; j < n; j++) {
+		suma = 0;
+		menor = 21;
+		for (int k = 1; k <= 4; k++) {
+			cout << "ingrese la nota: ";
+			cin >> nota;
+			suma = suma + nota;
+			if (nota < menor) {
+				menor = nota;
+
+			}
+
+
+		}
+
+		promedio = (suma - menor) / 3;
+		cout << "\n  el promedio:  " << j + 1 << "es: " << promedio << endl;
+	}
+
+
+	return 0;
+}
